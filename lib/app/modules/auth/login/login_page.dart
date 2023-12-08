ef1ca6d5f13287146cea201bf16e7f24ae6595a1
Ivan Francisco <ivantkd1@gmail.com>
2023-12-08 19:27:42 -0300

    Create componente textfield

--- lib/app/modules/auth/login/login_page.dart
+++ lib/app/modules/auth/login/login_page.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:pet_mobile/app/core/ui/widgets/pet_textform_field.dart';
 
 class LoginPage extends StatelessWidget {
 
-  const LoginPage({ super.key });
+  final testeEC = TextEditingController();
+
+ LoginPage({ super.key });
 
    @override
    Widget build(BuildContext context) {
        return Scaffold(
            appBar: AppBar(title: const Text('Login Pgae'),),
-           body: Container(),
+           body: Column(
+             children: [
+              PetTextformField(
+                label: 'Loign', 
+                obscureText: true,
+                controller: testeEC,),
+                Text(testeEC.text),
+             ],
+           ),
        );
   }
 }
