1f74637acc2a8878c82df7727fec217c5dfe12a9
Ivan Francisco <ivantkd1@gmail.com>
2023-12-10 18:58:15 -0300

    Update textform filed and insert icons

--- lib/app/modules/auth/login/login_page.dart
+++ lib/app/modules/auth/login/login_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:pet_mobile/app/core/ui/icons/pet_icons.dart';
 import 'package:pet_mobile/app/core/ui/widgets/pet_textform_field.dart';
 
 class LoginPage extends StatelessWidget {
 
   final testeEC = TextEditingController();
+  final formKey = GlobalKey<FormState>();
 
  LoginPage({ super.key });
 
@@ -11,14 +13,32 @@ class LoginPage extends StatelessWidget {
    Widget build(BuildContext context) {
        return Scaffold(
            appBar: AppBar(title: const Text('Login Pgae'),),
-           body: Column(
-             children: [
-              PetTextformField(
-                label: 'Loign', 
-                obscureText: true,
-                controller: testeEC,),
-                Text(testeEC.text),
-             ],
+           body: Padding(
+             padding: const EdgeInsets.all(8.0),
+             child: Form(
+              key: formKey,
+               child: Column(
+                 children: [
+                  PetTextformField(
+                    label: 'Loign', 
+                    obscureText: true,
+                    controller: testeEC,
+                    validator: (String? value){
+                      if(value == null || value.isEmpty){
+                        return 'Valor Obrigatório';
+                      }
+                      return null;
+                    },
+                    ),
+                    Text(testeEC.text),
+                    ElevatedButton(onPressed: (){
+                      formKey.currentState?.validate();
+                      print(testeEC.text);
+                    }, child: Text('Validar')),
+                    Icon(PetIcons.google, color: Colors.black),
+                 ],
+               ),
+             ),
            ),
        );
   }
